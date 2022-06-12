4a8eae5b094a40764ad8f984fd59f292e115bbfa
duhuhu <signup@duhuhu.tech>
2022-06-12 18:08:32 -0400

    Man made horrors beyond compreshension comprehension
    uwu

---
+++ KinematicBody2D.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _input(event):
+	if event.is_action_pressed("ui_left"):
+		pass
+	if event.is_action_pressed("ui_right"):
+		pass
